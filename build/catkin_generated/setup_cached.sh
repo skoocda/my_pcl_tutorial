0c1dc257b431929fea1865bfc5fc6aadff9de2b3
skoocda <dlloyd.cda@gmail.com>
2015-11-20 15:34:12 -0500

    First commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/skoocda/my_pcl_tutorial/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/skoocda/my_pcl_tutorial/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/skoocda/my_pcl_tutorial/devel/lib:/home/skoocda/my_pcl_tutorial/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/skoocda/my_pcl_tutorial/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/skoocda/my_pcl_tutorial/devel/lib/pkgconfig:/home/skoocda/my_pcl_tutorial/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/skoocda/my_pcl_tutorial/build"
+export PYTHONPATH="/home/skoocda/my_pcl_tutorial/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/skoocda/my_pcl_tutorial/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/skoocda/my_pcl_tutorial/src:$ROS_PACKAGE_PATH"
---
+++ build/my_pcl_tutorial/catkin_generated/package.cmake
@@ -0,0 +1,7 @@
+set(_CATKIN_CURRENT_PACKAGE "my_pcl_tutorial")
+set(my_pcl_tutorial_MAINTAINER "skoocda <skoocda@todo.todo>")
+set(my_pcl_tutorial_DEPRECATED "")
+set(my_pcl_tutorial_VERSION "0.0.0")
+set(my_pcl_tutorial_BUILD_DEPENDS "pcl_conversions" "pcl_ros" "roscpp" "sensor_msgs" "libpcl-all-dev")
+set(my_pcl_tutorial_RUN_DEPENDS "libpcl-all" "pcl_conversions" "pcl_ros" "roscpp" "sensor_msgs")
+set(my_pcl_tutorial_BUILDTOOL_DEPENDS "catkin")
